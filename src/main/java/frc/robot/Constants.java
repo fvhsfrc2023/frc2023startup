0bd4c4323395d90344dbaea1c7c9cced85249d5d
John Bonjour <whitewolfzhang@gmail.com>
2023-09-20 21:47:59 -0700

    Updated Constants.
    1. Renamed partial Motors constants;
    2. Added Joysticks constants;

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,9 +7,21 @@ public class Constants {
         public static final int kFrontRightChannel = 3;
         public static final int kRearRightChannel = 2;
 
-        public static final boolean kFrontLeftInverted = false;
-        public static final boolean kRearLeftInverted = false;
-        public static final boolean kFrontRightInverted = true;
-        public static final boolean kRearRightInverted = true;
+        public static final boolean bFrontLeftInverted = false;
+        public static final boolean bRearLeftInverted = false;
+        public static final boolean bFrontRightInverted = true;
+        public static final boolean bRearRightInverted = true;
+    }
+
+    public static class Joystick {
+        public static final int kJoyStickPort = 0;
+        
+        public static final int kXChannel = 0;
+        public static final int kYChannel = 1;
+        public static final int kZChannel = 4;
+
+        public static final double fXDeadZone = 0.1;
+        public static final double fYDeadZone = 0.1;
+        public static final double fZDeadZone = 0.1;
     }
 }
