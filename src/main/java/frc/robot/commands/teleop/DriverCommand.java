66ff1296a5dae1deacde40953ab7284a915b1733
John Bonjour <whitewolfzhang@gmail.com>
2023-09-20 21:50:25 -0700

    Created DriverCommand class.
    1. Extended from CommandBase;
    2. Used with DriverSystem(SubsystemBase);
    3. Used during teleop;

---
+++ src/main/java/frc/robot/commands/teleop/DriverCommand.java
@@ -0,0 +1,45 @@
+package frc.robot.commands.teleop;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.subsystems.DriverSystem;
+
+public class DriverCommand extends CommandBase {
+    private final DriverSystem m_DriverSystem;
+    private final Supplier<Double> getSpeed;
+    private final Supplier<Double> getOffset;
+
+    public DriverCommand(DriverSystem driverSystem, Supplier<Double> getSpeed, Supplier<Double> getOffset) {
+        this.m_DriverSystem = driverSystem;
+        this.getSpeed = getSpeed;
+        this.getOffset = getOffset;
+
+        addRequirements(m_DriverSystem);
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+        m_DriverSystem.drive(getSpeed.get(), getOffset.get());
+
+        SmartDashboard.putNumber("getSpeed", getSpeed.get());
+        SmartDashboard.putNumber("getOffset", getOffset.get());
+    }
+
+    @Override
+    public void end(boolean interupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
