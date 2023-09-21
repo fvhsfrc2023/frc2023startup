21dd6548cefd54fadde23e9500b1634c6870b672
John Bonjour <whitewolfzhang@gmail.com>
2023-09-20 21:49:19 -0700

    Created Controller class.

---
+++ src/main/java/frc/robot/Controller.java
@@ -0,0 +1,30 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+import frc.robot.Constants;
+
+public class Controller {
+    private final Joystick m_Joystick;
+
+
+    public Controller() {
+        m_Joystick = new Joystick(Constants.Joystick.kJoyStickPort);
+        
+        m_Joystick.setXChannel(Constants.Joystick.kXChannel);
+        m_Joystick.setYChannel(Constants.Joystick.kYChannel);
+        m_Joystick.setZChannel(Constants.Joystick.kZChannel);
+    }
+
+    public double getVerticleOffset() {
+        return Math.abs(m_Joystick.getY()) > Constants.Joystick.fYDeadZone ? m_Joystick.getY() : 0;
+    }
+
+    public double getHorizontalOffset() {
+        return Math.abs(m_Joystick.getX()) > Constants.Joystick.fYDeadZone ? m_Joystick.getX() : 0;
+    }
+
+    public double getRotation() {
+        return Math.abs(m_Joystick.getZ()) > Constants.Joystick.fYDeadZone ? m_Joystick.getZ() : 0;
+    }
+}
